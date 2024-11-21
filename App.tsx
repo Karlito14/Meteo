8d6e35009331db75b9137d7ab5f7f66bfb996ff8
Karlito14 <leiroz26@hotmail.com>
2024-11-21 09:57:46 +0100

    init du projet

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { Home } from './pages/Home/Home';
+import { SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Home />
+    </SafeAreaView>
+  );
+}
---
+++ pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { styles } from './Home.style';
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
