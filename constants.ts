0535df8a49bed0081f61b782ed65187ae4e87f03
Karlito14 <leiroz26@hotmail.com>
2024-11-24 11:56:32 +0100

    récuperer la localisation utilisateur

---
+++ constants.ts
@@ -0,0 +1,14 @@
+const PARIS_LOCATION = {
+  timestamp: Date.now(),
+  coords: {
+    accuracy: null,
+    altitude: null,
+    altitudeAccuracy: null,
+    heading: null,
+    latitude: 48.85,
+    longitude: 2.35,
+    speed: null,
+  },
+};
+
+export { PARIS_LOCATION };
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -2,8 +2,30 @@ import { MeteoAdvanced } from '@components/MeteoAdvanced';
 import { MeteoBasic } from '@components/MeteoBasic';
 import { SearchBar } from '@components/SearchBar';
 import { StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+import { PARIS_LOCATION } from '@constants';
 
 export const Home = () => {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setLocation(PARIS_LOCATION);
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
       <MeteoBasic />
