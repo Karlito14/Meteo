04c96e61ce2e6eaece23b013e96cced47fa452bb
Karlito14 <leiroz26@hotmail.com>
2024-12-05 14:02:30 +0100

    affichage header et retour en arriere

--- pages/Forecast.tsx
+++ pages/Forecast.tsx
@@ -1,13 +1,47 @@
 import { Container } from '@components/Container';
 import { Txt } from '@components/Txt';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 
 export const Forecast = () => {
   const { params } = useRoute();
+  const navigation = useNavigation();
+
   console.log(params);
-  return (
-    <Container>
-      <Txt>Forecast</Txt>;
-    </Container>
+
+  const backButton = (
+    <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+      <Txt>{'<'}</Txt>
+    </TouchableOpacity>
   );
+
+  const header = (
+    <View style={styles.container}>
+      {backButton}
+      <View style={styles.content}>
+        <Txt>{params?.city}</Txt>
+        <Txt style={styles.subtitle}>Prévisions de la semaine</Txt>
+      </View>
+    </View>
+  );
+
+  return <Container>{header}</Container>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  button: {
+    width: 30
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'blue',
+    marginRight: 30,
+  },
+  subtitle: {
+    fontSize: 20
+  },
+});
